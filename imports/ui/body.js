b5dae5fa827da1f27d0bb1dcfb9ada09a4431d92
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-28 20:22:21 -0600

    test looks good.  need to test session, JSPath access, update JSON.

---
+++ imports/ui/body.js
@@ -0,0 +1,15 @@
+import { Template } from 'meteor/templating';
+import './body.html';
+const raw_data = require('/imports/api/IPIP120.json');
+
+Template.body.helpers({
+
+//  questions: [
+//    { question: 'This is task 1' },
+//    { question: 'This is task 2' },
+//    { question: 'This is task 3' },
+    //  ],
+    questions: raw_data['value'],
+
+});
+
