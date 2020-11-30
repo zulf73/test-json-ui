338849e4666b7a9d808002c117d1ea543aedbef3
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-30 01:20:07 -0600

    json_set looks good

--- tests/test.js
+++ tests/test.js
@@ -16,15 +16,14 @@ function json_set( path, doc, val ){
     if (keys[0]==''){
 	keys = keys.slice(1,n+1);
     }
-    console.log(keys);
     if (n == 1 ){
 	doc[keys[0]] = val;
 	return doc;
     }
-    var shorter_path = keys.slice(1,n).join('.');
-    return json_set( shorter_path, doc[keys[0]], val);
+    doc[keys[0]] = json_set( keys.slice(1,n).join('.'), doc[keys[0]], val);
+    return doc;
 }
 
 var new_doc = json_set( '.value.5.answers.3.type', raw_data, '25');
-console.log( new_doc );
-//console.log( new_doc['value'][5]['answers'] );
+//console.log( new_doc );
+console.log( new_doc['value'][5] );
