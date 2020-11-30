ba543604bc31ee3e61abe0bcba2e1ec0426f3b58
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-29 23:38:04 -0600

    test/test.js has json_set

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,23 @@
 var raw_data = require('./IPIP120.json');
 
 console.log( raw_data['value']);
+
+function get_reference( path, doc ) {
+    var keys = path.split('.');
+    var n = keys.length;
+    if (n == 1 ){
+	return doc[keys[0]];
+    }
+    return get_reference( keys.slice(1,n).join('.'), doc[keys[0]]);
+}
+function json_set( path, doc, val ){
+    var keys = path.split('.');
+    var n = keys.length;
+    if (n == 1 ){
+	doc[keys[0]] = val;
+    }
+    return json_set( keys.slice(1,n).join('.'), doc[keys[0]], val);
+}
+
+var new_doc = json_set( '.value.5.answers.3', raw_data, '25');
+console.log( new_doc['value'][5]['answers'] );
