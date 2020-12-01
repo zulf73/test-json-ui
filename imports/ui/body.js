86c4142c4ac38f0b9348394c19035580a9dfa550
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-30 18:50:37 -0600

    blaze-bs4 not loading properly.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,6 +2,20 @@ import { Template } from 'meteor/templating';
 import './body.html';
 const raw_data = require('/imports/api/IPIP120.json');
 
+
+
+import 'meteor/jkuester:blaze-bs4'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css' // this is the default BS theme as example
+import popper from 'popper.js'
+global.Popper = popper // fixes some issues with Popper and Meteor
+
+
+import { BlazeBs4 } from 'meteor/jkuester:blaze-bs4'
+
+BlazeBs4.button.load()
+  .then(loaded => console.log('button template loaded'));
+
 Template.body.helpers({
 
 //  questions: [
